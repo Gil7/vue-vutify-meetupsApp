a9b11856a3122f25615ecd0aed858899cb47487f
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2017-08-22 15:15:38 -0500

    :v: filter fixed, pass the value to date object :D

--- src/filters/date.js
+++ src/filters/date.js
@@ -1,6 +1,6 @@
 export default (value) => {
-    const date = new Date()
-    return date.toLocaleString(['es-mx'], 
+    const date = new Date(value)
+    return date.toLocaleString(['en-US'], 
     {
         month: 'short', 
         day: '2-digit', 
