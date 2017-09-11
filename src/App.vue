e1899ebf9014aa6a88aad218b23ae9577a92177a
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2017-09-11 14:02:28 -0500

    :clipboard: check if the user is authenticated and display navbar

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,14 @@
             {{item.title}}
           </v-list-tile-content>
         </v-list-tile>
+        <v-list-tile v-if="userIsAuthenticated" @click="onLogout">
+          <v-list-tile-action>
+            <v-icon>exit_to_app</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            Logout
+          </v-list-tile-content>
+        </v-list-tile>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar class="primary" dark>
@@ -34,6 +42,12 @@
               {{item.icons}}
             </v-icon>
             {{item.title}}
+            </v-btn>
+            <v-btn flat v-if="userIsAuthenticated" @click="onLogout">
+              <v-icon>
+                exit_to_app
+              </v-icon>
+              Logout
             </v-btn>
         </v-toolbar-items>
     </v-toolbar>
@@ -67,6 +81,11 @@
       },
       userIsAuthenticated () {
         return this.$store.getters.user !== null && this.$store.getters.user !== undefined
+      }
+    },
+    methods: {
+      onLogout () {
+        this.$store.dispatch('logout')
       }
     }
   }
